dcfe8f508db8f15d580947426c1e8f0fa0d3d4ac
ProximoBinks <elliot.koh@student.adelaide.edu.au>
2025-04-02 02:28:12 +1030

    Update contact.js

--- pages/contact.js
+++ pages/contact.js
@@ -35,7 +35,7 @@ export default function Contact() {
       </Head>
 
       <div className="max-w-4xl mx-auto pt-[5rem] xl:pt-[10rem] px-4 sm:px-6 lg:px-8">
-        <div className="bg-white rounded-2xl p-8 shadow-xl ring-1 ring-gray-100">
+        <div className="bg-white rounded-2xl p-8 shadow-xl ring-1 ring-gray-100 mb-5">
           <div className="max-w-2xl mx-auto">
             <h1 className="text-4xl font-bold text-center mb-2 bg-gradient-to-r from-gray-900 to-gray-700 bg-clip-text text-transparent">
               Get in Touch
